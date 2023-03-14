2d47f982f537d56ab22666606ac3f24f456812f7
Mehul Kar <mehul.kar@vercel.com>
2023-03-13 22:53:01 -0700

    Run all examples in same Github Action run
    Compiling turbo is the slowest part of running these tests in CI, so instead of doing
    it 18 times, we can do it once and run the tests all together. We can attempt to parallelize
    the tests instead.

--- scripts/run-example.sh
+++ scripts/run-example.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
-
 set -e
 
+## Usage
+# ./scripts/run-examples <folder-name> <package-manager>
+# Examples
+# ./scripts/run-examples basic yarn
+
+## Or run all tests without any arguments
+# ./scripts/run-examples
+
+# Setup prysk
 python3 -m venv .cram_env
-.cram_env/bin/pip install prysk
+
+.cram_env/bin/python3 -m pip install --quiet --upgrade pip
+echo "Installing prysk..."
+.cram_env/bin/pip3 install --quiet prysk
 
 export folder=$1
 export pkgManager=$2
 
-TEST_FILE="examples_tests/$2-$1.t"
+# If both arguments were provided, we'll try to run a specific test
+if [ -n "$1" ] && [ -n "$2" ]; then
+  TEST_FILE="examples_tests/$2-$1.t"
+  if [ -f "$TEST_FILE" ]; then
+    echo "Running $TEST_FILE"
+    .cram_env/bin/prysk --shell="$(which bash)" "$TEST_FILE"
+  else
+    echo "Could not find $TEST_FILE"
+    exit 1
+  fi
 
-if [ -f "$TEST_FILE" ]; then
-  echo "Running $TEST_FILE"
-  .cram_env/bin/prysk --shell="$(which bash)" "$TEST_FILE"
-else
-  echo "Could not find $TEST_FILE"
-  exit 1
+  # exit if both args were provided and we haven't already exited
+  exit
 fi
+
+echo "No arguments provided, running all tests"
+.cram_env/bin/prysk --shell="$(which bash)" "$PWD/examples_tests/"
