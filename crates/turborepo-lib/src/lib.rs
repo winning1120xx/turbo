af168deb7c60f3f3b24905b2baee1d6388d62297
Leah <github.leah@hrmny.sh>
2023-03-15 18:31:35 +0100

    use compile error for feature validation

--- crates/turborepo-lib/src/lib.rs
+++ crates/turborepo-lib/src/lib.rs
@@ -39,3 +39,9 @@ pub fn main() -> Payload {
         }
     }
 }
+
+#[cfg(all(feature = "native-tls", feature = "rustls-tls"))]
+compile_error!("You can't enable both the `native-tls` and `rustls-tls` feature.");
+
+#[cfg(all(not(feature = "native-tls"), not(feature = "rustls-tls")))]
+compile_error!("You have to enable one of the TLS features: `native-tls` or `rustls-tls`");
