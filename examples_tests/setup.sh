ee8c50be1da77384502e1900db53830bd1bd5e86
Mehul Kar <mehul.kar@vercel.com>
2023-03-17 11:40:28 -0700

    Back to shared script, but add a feature

--- examples_tests/setup.sh
+++ examples_tests/setup.sh
@@ -47,9 +47,5 @@ fi
 # Setup Git things (this is similar to integration_tests/setup_git.sh, but doesn't touch npm)
 # # Delete .git directory if it's there, we'll set up a new git repo
 [ ! -d .git ] || rm -rf .git
-git init "$TARGET_DIR" --quiet --initial-branch=main
-GIT_ARGS="--git-dir=${TARGET_DIR}/.git --work-tree=${TARGET_DIR}"
-git ${GIT_ARGS} config user.email "turbo-test@example.com"
-git ${GIT_ARGS} config user.name "Turbo Test"
-git ${GIT_ARGS} add .
-git ${GIT_ARGS} commit -m "Initial" --quiet
+# Second arg passed is false, which will skip the npm install in setup_git.sh
+"${SCRIPT_DIR}/../cli/integration_tests/setup_git.sh" "${TARGET_DIR}" "false"
